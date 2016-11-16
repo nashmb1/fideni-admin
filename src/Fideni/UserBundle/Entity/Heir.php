2e4de8e4bec882b97edab5996cb55e75b7f3fa28
HAROUNA MAYAOU <hm.nassirou@gmail.com>
2016-11-16 18:42:36 +0100

    debut config admin

---
+++ src/Fideni/UserBundle/Entity/Heir.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Fideni\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Heir
+ *
+ * @ORM\Table(name="heir")
+ * @ORM\Entity(repositoryClass="Fideni\UserBundle\Repository\HeirRepository")
+ */
+class Heir
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="surname", type="string", length=255)
+     */
+    private $surname;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="User", cascade={"persist", "remove"} )
+     */
+    protected $user;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Heir
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     * @return Heir
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string 
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+}
