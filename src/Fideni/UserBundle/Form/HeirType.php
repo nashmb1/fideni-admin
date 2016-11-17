f8b45b76de848816c2aa0c8779e96fc8a077372c
HAROUNA MAYAOU <hm.nassirou@gmail.com>
2016-11-17 18:31:03 +0100

    add user config

---
+++ src/Fideni/UserBundle/Form/HeirType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Fideni\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class HeirType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name')->add('surname')->add('user')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Fideni\UserBundle\Entity\Heir'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'fideni_userbundle_heir';
+    }
+
+
+}
