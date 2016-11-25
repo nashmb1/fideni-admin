7e03f4040ef58dbc336cbb01aca421e84c4e6081
HAROUNA MAYAOU <hm.nassirou@gmail.com>
2016-11-25 18:10:55 +0100

    success redirect

--- src/Fideni/CoreBundle/Controller/AdminController.php
+++ src/Fideni/CoreBundle/Controller/AdminController.php
@@ -9,25 +9,51 @@
 namespace Fideni\CoreBundle\Controller;
 
 use Fideni\CoreBundle\Entity\Subscription;
+use Fideni\CoreBundle\Form\SubscriptionType;
 use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
 
 class AdminController extends BaseAdminController
 {
+
+//    public function subscriptionAction(){
+//        $easyadmin = $this->request->attributes->get('easyadmin');
+//        $entity = $easyadmin['item'];
+////dump($entity,$easyadmin);die;
+//        $subscription = new Subscription();
+//        $subscription->setUser($entity);
+//        $form = $this->createForm(new SubscriptionType(), $subscription);
+//
+//        $return = $this->redirectToRoute('fideni_core_homepage',[
+//            'entity' => 'Subscription',
+//            'form' => $form->createView()
+//        ]);
+//        dump($return);die;
+//        return $this->render('@FideniCore/subscription.html.twig', [
+//            'form' => $form->createView()
+//        ]);
+//    }
     public function subscriptionAction()
     {
         $this->entity = $this->get('easyadmin.config.manager')->getEntityConfiguration('Subscription');
-//        $newForm = $this->executeDynamicMethod('create<EntityName>NewForm', array($entity, $fields));
         $easyadmin = $this->request->attributes->get('easyadmin');
         $entity = $easyadmin['entity'];
         $action = $easyadmin['view'];
 //        $fields = $entity[$action]['fields'];
+        $easyadmin['view'] = 'new';
 
-//        dump($entity,$action,$this->entity);die;
         $entity = $easyadmin['item'];
-
+        $template = $this->entity['templates']['new'];
+//        dump($template);die;
 //        $entity= $this->entity;
         $subscription = new Subscription();
         $subscription->setUser($entity);
+
+        $easyadmin['entity'] = $this->entity;
+//                dump($easyadmin,$entity,$action,$this->entity,$subscription);die;
+
+        $this->request->attributes->set('easyadmin', $easyadmin);
+
+
         $fields = $this->entity['new']['fields'];
         $form = $this->createFormBuilder($subscription)
                       ->add('user', 'entity', array(
@@ -40,12 +66,18 @@ class AdminController extends BaseAdminController
 //        $subscription->setCampaignNumber(555);
         $return = $this->redirectToRoute('easyadmin', array(
             'action' => 'new',
-//            'entity' => 'Subscription'
-            'form'   => $form,
+            'entity' => 'Subscription',
+//            'form'   => $form->createView(),
 //            'entity' => $entity,
 //            'entity_fields' => $fields
         ));
-//        return $return;
+        $newForm = $this->createForm(new SubscriptionType(), $subscription);
+        return $this->render($this->entity['templates']['new'], array(
+            'form' => $form->createView(),
+            'entity_fields' => $fields,,
+            'entity' => $entity
+        ));
+        return $return;
         dump($return);die;
 //        $this->
 //        return $this->redire
