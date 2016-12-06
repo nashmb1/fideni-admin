f47d0791cdf33e5c6950e8d9987500c6480b1653
HAROUNA MAYAOU <hm.nassirou@gmail.com>
2016-12-06 18:05:58 +0100

    add ajax route

--- src/Fideni/CoreBundle/Controller/AjaxController.php
+++ src/Fideni/CoreBundle/Controller/AjaxController.php
@@ -6,8 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AjaxController extends Controller
 {
+    public function getGlobalStatsAction(){
+
+    }
+
     public function projectsAction()
     {
+
+    }
+
+    public function getAllUsersAction(){
+
+    }
+
+    public function getAllProjectsAction($status){
+
+    }
+
+    public function getUserShareAction(){
 
     }
 
