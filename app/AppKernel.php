7a80e58fe52c5feb720ea02c844fd5635f8fcfb7
HAROUNA MAYAOU <hm.nassirou@gmail.com>
2017-01-26 17:13:02 +0100

    styling users_list

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new \JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
             new Fideni\CoreBundle\FideniCoreBundle(),
+            new \Vich\UploaderBundle\VichUploaderBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
